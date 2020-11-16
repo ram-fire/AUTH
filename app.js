af0123ed8628b0185189fca6b117273271d791eb
ram-fire <sachinrajpootji@gmail.com>
2020-11-16 23:17:09 +0530

    second commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 
 mongoose.connect("mongodb://localhost:27017/userDB",
@@ -25,7 +25,6 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:["password"]});
 
 const user=new mongoose.model("user",userSchema);
 
@@ -39,7 +38,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new user({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(err) console.log(err);
@@ -59,7 +58,7 @@ app.post("/login",function(req,res){
         {
             if(foundUser)
             {
-                if(foundUser.password===req.body.password)
+                if(foundUser.password===md5(req.body.password))
                 {
                     console.log("logged in")
                     res.render("secrets");
